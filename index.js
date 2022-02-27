0dc38b30cbc9240d44f85e88470d9046821edcf8
mio kataoka <mio.kanna@gmail.com>
2022-02-26 20:17:32 -0800

    commit tree 58bfebd18abe44da81766c3e61ff1d0bff09aa24

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = [`Milo`, `Otis`, `Garfield`];
+function destructivelyAppendCat(newCat) {
+    cats.push(newCat);
+}
+function destructivelyPrependCat(newCat){
+    cats.unshift(newCat);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+  const moreCats = [...cats];
+  moreCats.push(name);
+  return moreCats;
+}
+function prependCat(name){
+    const evenMoreCats = [...cats];
+    evenMoreCats.unshift(name);
+    return evenMoreCats;
+}
+function removeLastCat() {
+   const minusTheLastCat = [...cats];
+   minusTheLastCat.splice(minusTheLastCat.length-1,1)
+   return minusTheLastCat;
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
